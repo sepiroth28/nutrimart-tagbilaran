82103ee4b9b5bf114c790f129e348a35ab94aba4
unknown <rodrigo.m.flores.jr@gmail.com>
2012-09-06 12:41:51 +0800

    stable quickreport for customer2

---
+++ STG_Milling/dump file/tables dump/store_settings.sql
@@ -0,0 +1,7 @@
+﻿DROP TABLE IF EXISTS `dbinventory`.`store_settings`;
+CREATE TABLE  `dbinventory`.`store_settings` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `store_name` varchar(45) NOT NULL,
+  `activated` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
