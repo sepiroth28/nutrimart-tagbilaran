67cdcaae740a0a49942da97c1f75769c65cee358
unknown <NutrimartCalape@Nutrimart-Calap.(none)>
2012-10-03 16:54:01 +0800

    last commit as of now

---
+++ STG_Milling/dump file/tables dump/customer_reference.sql
@@ -0,0 +1,10 @@
+﻿DROP TABLE IF EXISTS `dbinventory`.`cutomer_reference`;
+CREATE TABLE  `dbinventory`.`cutomer_reference` (
+  `address` varchar(120) NOT NULL,
+  `reference` varchar(100) NOT NULL,
+  `work` varchar(50) NOT NULL,
+  `store_name` varchar(50) NOT NULL,
+  `cus_id` int(10) unsigned NOT NULL,
+  KEY `FK_cutomer_reference_1` (`cus_id`),
+  CONSTRAINT `FK_cutomer_reference_1` FOREIGN KEY (`cus_id`) REFERENCES `customers` (`customers_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
---
+++ STG_Milling/dump file/tables dump/verified_customer.sql
@@ -0,0 +1,7 @@
+﻿DROP TABLE IF EXISTS `dbinventory`.`verified_customer`;
+CREATE TABLE  `dbinventory`.`verified_customer` (
+  `customers_id` int(10) unsigned NOT NULL,
+  `verefied` int(10) unsigned NOT NULL,
+  KEY `FK_verified_customer_1` (`customers_id`),
+  CONSTRAINT `FK_verified_customer_1` FOREIGN KEY (`customers_id`) REFERENCES `customers` (`customers_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
